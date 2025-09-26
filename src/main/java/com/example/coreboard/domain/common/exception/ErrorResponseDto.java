d4ca596ae3c0b29a8a6133b1d12a73a214ed338e
hansolChoi29 <ggksthf29@gmail.com>
2025-09-26 23:26:59 +0900

    feat: 게터 추가

--- src/main/java/com/example/coreboard/domain/common/exception/ErrorResponseDto.java
+++ src/main/java/com/example/coreboard/domain/common/exception/ErrorResponseDto.java
@@ -3,4 +3,16 @@ package com.example.coreboard.domain.common.exception;
 public class ErrorResponseDto {
     private int status;
     private String message;
+
+    public ErrorResponseDto(int status, String message){
+        this.status=status;
+        this.message=message;
+    }
+
+    public int getStatus(){
+        return status;
+    }
+    public String getMessage(){
+        return message;
+    }
 }
