7f5bba97d847a7e3a92bf1f160502099dd726462
hansolChoi29 <ggksthf29@gmail.com>
2025-09-27 01:49:42 +0900

    feat: 글로벌익셉션 개선하여 응답바디에 의도된 대로 status, message 나오는 거 확인함

--- src/main/java/com/example/coreboard/domain/common/exception/GlobalException.java
+++ src/main/java/com/example/coreboard/domain/common/exception/GlobalException.java
@@ -1,11 +1,18 @@
 package com.example.coreboard.domain.common.exception;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
+@RestControllerAdvice
 public class GlobalException {
 
     @ExceptionHandler(RuntimeException.class)
-    public Object exception(RuntimeException e){
-        return e.getMessage();
+    public ResponseEntity<ErrorResponseDto> handleMemberException(ErrorException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(new ErrorResponseDto(e.getStatus(), e.getMessage())
+                );
     }
 }
