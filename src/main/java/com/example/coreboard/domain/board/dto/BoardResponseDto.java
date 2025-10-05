3c191acb8cd875fbdeb7848ae34462312948c717
hansolChoi29 <ggksthf29@gmail.com>
2025-10-05 18:11:03 +0900

    feat: 단건조회 메서드 생성

---
+++ src/main/java/com/example/coreboard/domain/board/dto/BoardResponseDto.java
@@ -0,0 +1,7 @@
+package com.example.coreboard.domain.board.dto;
+
+public class BoardResponseDto {
+    // 응답으로 나와야 하는 것
+    String boardTitle;
+    String boardContents;
+}
--- src/main/java/com/example/coreboard/domain/board/service/Service.java
+++ src/main/java/com/example/coreboard/domain/board/service/Service.java
@@ -1,4 +1,11 @@
 package com.example.coreboard.domain.board.service;
 
+import com.example.coreboard.domain.board.repository.BoardRepository;
+
 public class Service {
+    private final BoardRepository boardRepositody;
+
+    public Service(BoardRepository boardRepositody) {
+        this.boardRepositody = boardRepositody;
+    }
 }
