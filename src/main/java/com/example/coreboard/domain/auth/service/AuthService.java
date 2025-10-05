0b259835942dd4c45f4f54652c83be870e1ad612
hansolChoi29 <ggksthf29@gmail.com>
2025-10-06 00:29:43 +0900

    test: 서버에러와 사용자 실수로 인한 실패 에러 test 코드

--- src/main/java/com/example/coreboard/domain/auth/service/AuthService.java
+++ src/main/java/com/example/coreboard/domain/auth/service/AuthService.java
@@ -30,23 +30,23 @@ public class AuthService {
         this.userRepository = userRepository;
     }
 
-    @Transactional
-    public SignUpResponse signup(SignUpRequest signupRequest) {
-        // 트러블: getter인데 파라미터를 넣어서 에러났었음
-        //signupRequest.getUsername(username)
-        if (authRepository.existsByUsername(signupRequest.getUsername())) {
-            throw new AuthErrorException(CONFLICT);
-        }
-        byte[] salt=passwordEncoder();
-        String encodePassword = passwordEncoder.encrypt(signupRequest.getPassword(), salt);
-
-        Users newUser = new Users(
-                signupRequest.getUsername(),
-                encodePassword
-        );
-        authRepository.save(newUser);
-        return new SignUpResponse(newUser.getUsername(), "가입 성공!");
-    }
+//    @Transactional
+//    public SignUpResponse signup(SignUpRequest signupRequest) {
+//        // 트러블: getter인데 파라미터를 넣어서 에러났었음
+//        //signupRequest.getUsername(username)
+//        if (authRepository.existsByUsername(signupRequest.getUsername())) {
+//            throw new AuthErrorException(CONFLICT);
+//        }
+//        byte[] salt=passwordEncoder();
+//        String encodePassword = passwordEncoder.encrypt(signupRequest.getPassword(), salt);
+//
+//        Users newUser = new Users(
+//                signupRequest.getUsername(),
+//                encodePassword
+//        );
+//        authRepository.save(newUser);
+//        return new SignUpResponse(newUser.getUsername(), "가입 성공!");
+//    }
 
     // 검증을 한 번 더 하는 이유
     // 유틸은 단순히 JWT 구조/서명/만료 검증
@@ -57,30 +57,30 @@ public class AuthService {
     // 2) JWT 발급 - 로그인 성공 시 유틸 호출
     // 3) 응답 DTO 반환 - accessToken
 
-    @Transactional
-    public ResponseEntity<TokenResponse> signin(@RequestBody SignInRequest signinRequest) {
-        // 사용자 입력 정보 추출
-        String username = signinRequest.getUsername(); //  사용자 아이디
-        String inputPassword = signinRequest.getPassword(); // 사용자 비밀번호
-
-        // DB에서 사용자 조회
-        Users user = userRepository.findByUsername(username) //DB에 접근하여 아이디 조회
-                .orElseThrow(() -> new AuthErrorException(NOT_FOUND)); // 값이 없을 경우 에러 던짐, 있다면 값 반환
-        // 비밀번호 검증
-        if (!passwordEncoder.matches(inputPassword, user.getPassword())) {
-            // matches : PasswordEncoder객체가 제공하는 메서드여야 함
-            throw new AuthErrorException(BAD_REQUEST);
-        } else {
-            Long userId = user.getUserId(); // DB에서 사용자 ID 조회
-            String accessToken = JwtUtil.createAccessToken(userId, username); // 엑세스 토큰 발급
-            String refreshToken = JwtUtil.createRefreshToken(userId, username); // 엑세스 토큰 발급
-
-            // put은 문법상 맞지 않음
-            // tokenResponse.put("accessToken",accessToken);
-            // tokenResponse.put("refreshToken",refreshToken);
-
-            TokenResponse tokenResponse = new TokenResponse(accessToken, refreshToken); // 토큰 세터로 주입
-            return ResponseEntity.ok().body(tokenResponse); // 바디에 accessToken, refreshToken응답
-        }
-    }
+//    @Transactional
+//    public ResponseEntity<TokenResponse> signin(@RequestBody SignInRequest signinRequest) {
+//        // 사용자 입력 정보 추출
+//        String username = signinRequest.getUsername(); //  사용자 아이디
+//        String inputPassword = signinRequest.getPassword(); // 사용자 비밀번호
+//
+//        // DB에서 사용자 조회
+//        Users user = userRepository.findByUsername(username) //DB에 접근하여 아이디 조회
+//                .orElseThrow(() -> new AuthErrorException(NOT_FOUND)); // 값이 없을 경우 에러 던짐, 있다면 값 반환
+//        // 비밀번호 검증
+//        if (!passwordEncoder.matches(inputPassword, user.getPassword())) {
+//            // matches : PasswordEncoder객체가 제공하는 메서드여야 함
+//            throw new AuthErrorException(BAD_REQUEST);
+//        } else {
+//            Long userId = user.getUserId(); // DB에서 사용자 ID 조회
+//            String accessToken = JwtUtil.createAccessToken(userId, username); // 엑세스 토큰 발급
+//            String refreshToken = JwtUtil.createRefreshToken(userId, username); // 엑세스 토큰 발급
+//
+//            // put은 문법상 맞지 않음
+//            // tokenResponse.put("accessToken",accessToken);
+//            // tokenResponse.put("refreshToken",refreshToken);
+//
+//            TokenResponse tokenResponse = new TokenResponse(accessToken, refreshToken); // 토큰 세터로 주입
+//            return ResponseEntity.ok().body(tokenResponse); // 바디에 accessToken, refreshToken응답
+//        }
+//    }
 }
--- src/test/java/common/exception/TestController.java
+++ src/test/java/common/exception/TestController.java
@@ -1,13 +1,21 @@
 package common.exception;
 
 
+import com.example.coreboard.domain.common.response.ApiResponse;
 import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("글로벌과 공통 포맷이 잘 되는지 확인하는 테스트")
 public class TestController {
+    public static void main(String[] args) {
+        // 실패 - 사용자 실수 공통 포맷
+        ApiResponse<Void> responseFail = ApiResponse.fail(400, "로그인 실패!");
 
-   try{
-
+        System.out.println("status: " + responseFail.getStatus());
+        System.out.println("message: " + responseFail.getMessage());
+        
+        // 서버 에러 글로벌 공통 포맷
+        ApiResponse<Void> responseError = ApiResponse.error(500, "서버에러");
+        System.out.println("status: " + responseError.getStatus());
+        System.out.println("message: " + responseError.getMessage());
     }
-
 }
