913f59ca1747037e397d5cd789a2cd7a271e3118
hansolChoi29 <ggksthf29@gmail.com>
2025-10-06 00:55:39 +0900

    chore: 주석 추가

--- src/main/java/com/example/coreboard/domain/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/coreboard/domain/common/exception/GlobalExceptionHandler.java
@@ -11,20 +11,24 @@ public class GlobalExceptionHandler {
 
     // 성공 응답은 컨트롤에서 직접 반환하기 때문에 예외 처리기에서 다루지 않음
 
+    // ResponseEntity: HTTP 응답 껍데기(상태코드/헤더/바디)
+    // ApiResponse<Object> : 바디에 넣을 공통 포맷 {"message" : "","data" : {}}
+    
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiResponse<Object>> handleFailException(ErrorException e) {    // ResponseEntity는 HTTP 응답
-        // 객체고,
-                                                                                        // 안에 ApiResponse을 담겠다, Void는 status+message만 보내겠다.
+    public ResponseEntity<ApiResponse<Object>> handleFailException(ErrorException e) {
+        // ResponseEntity는 HTTP 응답 객체고,
+        // 안에 ApiResponse을 담겠다, Void는 status+message만 보내겠다.
         return ResponseEntity
-                .status(e.getStatus())                                  // 응답 상태코드 400, 404, 405 등
+                .status(e.getStatus())                   // 응답 상태코드 400, 404, 405 등
                 .body(ApiResponse.fail(e.getMessage())); // 에러 메시지와 응답 코드를 body에 담겠다.
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiResponse<Object>> handleErrorException(ErrorException e) {// ResponseEntity는 HTTP 응답 객체고,
-                                                                                    // 안에 ApiResponse을 담겠다, Void는 status+message만 보내겠다.
+    public ResponseEntity<ApiResponse<Object>> handleErrorException(ErrorException e) {
+        // ResponseEntity는 HTTP 응답 객체고,
+        // 안에 ApiResponse을 담겠다, Void는 status+message만 보내겠다.
         return ResponseEntity
-                .status(e.getStatus())                                      //500 서버 에러
+                .status(e.getStatus())                        //500 서버 에러
                 .body(ApiResponse.error(e.getMessage()));    // 에러 메시지와 응답 코드를 body에 담겠다.
     }
 }
