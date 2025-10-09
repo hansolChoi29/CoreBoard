5df60e1bad7725e18a032c3c0c3d996f50ab0caf
hansolChoi29 <ggksthf29@gmail.com>
2025-10-09 19:27:05 +0900

    feat: 회원가입 성공

--- src/main/java/com/example/coreboard/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/coreboard/domain/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.example.coreboard.domain.auth.dto.SignUpRequest;
 import com.example.coreboard.domain.common.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,21 +20,14 @@ public class AuthController {
         this.authService = authService;
     }
 
-    //ResponseEntity가 뭔데?
-    // HTTP응답 통째로 담는 상자.
-    // 상태코드, 헤더(Content-Type), 바디(JSON)
-
-    // 201: 새로운 리소스를 만들었다는 의미
-    // 로그인은 200
-
-    //    @PostMapping("/sign-in")
-//    public ResponseEntity<TokenResponse> signIn(@RequestBody SignInRequest signinRequest) {
-//        TokenResponse token = authService.signin(signinRequest).getBody(); //로그인 처리 후 토큰 발급
-//        return ResponseEntity.ok(token); // 토큰 응답
-//    }
-//    @PostMapping("/sign-up")
-//    public ResponseEntity<ApiResponse<SignUpResponse>> signUp(SignUpRequest request) {
-//        SignUpResponse signUpResponse = authService.signup(request);
-//        return ResponseEntity.ok(ApiResponse.ok(signUpResponse, "회원가입 성공"));
-//    }
+    // 트러블 : @RequestBody 누락 - 자바 객체로 변환 (httpMessageConverter)
+    @PostMapping("/sign-up")
+    public ResponseEntity<ApiResponse<SignUpResponse>> signUp(@RequestBody SignUpRequest request) {
+        // 요청의 JSON데이터를 SignUpResponse 객체로 바꿔서 받음 <= @RequestBody
+        // 응답은 ApiResponse<SignUpResponse> 형태로 감싸서 반환(공통 응답 포맷)
+        SignUpResponse signUpResponse = authService.signup(request);
+        // 서비스의 signup()메서드를 호출해서 실제 회원가입 로직 수행
+        return ResponseEntity.ok(ApiResponse.ok(signUpResponse, "회원가입 성공"));
+        // ApiResponse.ok는 공통 응답 포맷으로, 성공응답코드임
+    }
 }
