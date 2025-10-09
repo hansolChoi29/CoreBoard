36b0ae78bc1bf91822f3295b9c531977ae268ac4
hansolChoi29 <ggksthf29@gmail.com>
2025-10-09 15:33:56 +0900

    feat: PBKDF2의 보안취약점 발견하여 개선 전

--- src/main/java/com/example/coreboard/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/coreboard/domain/auth/controller/AuthController.java
@@ -1,21 +1,18 @@
 package com.example.coreboard.domain.auth.controller;
 
 
-import com.example.coreboard.domain.auth.dto.SignInRequest;
 import com.example.coreboard.domain.auth.dto.SignUpResponse;
-import com.example.coreboard.domain.auth.dto.TokenResponse;
 import com.example.coreboard.domain.auth.service.AuthService;
 import com.example.coreboard.domain.auth.dto.SignUpRequest;
+import com.example.coreboard.domain.common.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/auth")
 @RestController
 public class AuthController {
-
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -29,14 +26,14 @@ public class AuthController {
     // 201: 새로운 리소스를 만들었다는 의미
     // 로그인은 200
 
-//    @PostMapping("/sign-in")
+    //    @PostMapping("/sign-in")
 //    public ResponseEntity<TokenResponse> signIn(@RequestBody SignInRequest signinRequest) {
 //        TokenResponse token = authService.signin(signinRequest).getBody(); //로그인 처리 후 토큰 발급
 //        return ResponseEntity.ok(token); // 토큰 응답
 //    }
-
-//    @PostMapping("/sign-up")
-//    public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpRequest signupRequset) {
-//        return ResponseEntity.ok(authService.signup(signupRequset));
-//    }
+    @PostMapping("/sign-up")
+    public ResponseEntity<ApiResponse<SignUpResponse>> signUp(SignUpRequest request) {
+        SignUpResponse signUpResponse = authService.signup(request);
+        return ResponseEntity.ok(ApiResponse.ok(signUpResponse, "회원가입 성공"));
+    }
 }
