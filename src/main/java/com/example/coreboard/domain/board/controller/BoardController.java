4630787c8e89f32ebb41c01ae701d738e7a9387e
hansolChoi29 <ggksthf29@gmail.com>
2025-10-11 03:19:09 +0900

    feat: 단건조회

--- src/main/java/com/example/coreboard/domain/board/controller/BoardController.java
+++ src/main/java/com/example/coreboard/domain/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.coreboard.domain.board.controller;
 
 import com.example.coreboard.domain.board.dto.BoardRequest;
 import com.example.coreboard.domain.board.dto.BoardApiResponse;
+import com.example.coreboard.domain.board.dto.BoardResponse;
 import com.example.coreboard.domain.board.service.BoardService;
 import com.example.coreboard.domain.common.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,18 @@ public class BoardController {
             @RequestAttribute("username") String username   // 인터셉터의 username 이용
     ) {
         BoardApiResponse responseDto = boardService.createBoard(boardRequestDto, username); // title과 contents, 
-                                                                                            // uesrname 같이 응답하기 위함
+        // uesrname 같이 응답하기 위함
         return ResponseEntity.ok(ApiResponse.ok(responseDto, "게시글이 성공적으로 생성되었습니다."));
+    }
+
+    // 보드 단건조회
+    @GetMapping("/{boardId}")
+    public ResponseEntity<ApiResponse<BoardResponse>> getOneBoard(
+            @RequestAttribute("username") String username, // 유저 id 뽑아와서 권한체크
+            @PathVariable Long boardId                      // 단건 조회라서 id 받게 함
+    ) {
+        BoardResponse responseDto = boardService.findOneBoard(username, boardId);    // 유저id와 게시글id findOneBoard 실행하여
+                                                                                     // 반환된 값 변수에 넣음
+        return ResponseEntity.ok(ApiResponse.ok(responseDto, "게시글 조회!"));
     }
 }
--- src/main/java/com/example/coreboard/domain/board/service/BoardService.java
+++ src/main/java/com/example/coreboard/domain/board/service/BoardService.java
@@ -3,9 +3,15 @@ package com.example.coreboard.domain.board.service;
 
 import com.example.coreboard.domain.board.dto.BoardRequest;
 import com.example.coreboard.domain.board.dto.BoardApiResponse;
+import com.example.coreboard.domain.board.dto.BoardResponse;
 import com.example.coreboard.domain.board.entity.Board;
 import com.example.coreboard.domain.board.repository.BoardRepository;
+import com.example.coreboard.domain.common.exception.auth.AuthErrorException;
+import com.example.coreboard.domain.common.exception.board.BoardErrorException;
 import org.springframework.stereotype.Service;
+
+import static com.example.coreboard.domain.common.exception.auth.AuthErrorCode.*;
+import static com.example.coreboard.domain.common.exception.board.BoardErrorCode.*;
 
 @Service
 public class BoardService {
@@ -29,5 +35,24 @@ public class BoardService {
         );
         boardRepository.save(board); // 저장
         return new BoardApiResponse(board.getBoardTitle(), "게시글이 성공적으로 생성되었습니다.");
+    }
+
+    // 보드 단건 조회
+    public BoardResponse findOneBoard(
+            String username,
+            Long boardId
+    ) {
+        Board board = boardRepository.findById(boardId) // id 추출하는 메서드 이용해서
+                .orElseThrow(() -> new BoardErrorException(POST_NOT_FOUND)); // 값이 있으면 반환 없으면 에러 던짐
+
+        if (!board.getUsername().equals(username)) { // 권한 체크
+            throw new AuthErrorException(FORBIDDEN);
+        }
+
+        // 트러블 - board만 넣었더니 500 에러: 단건 조회용, 타이틀과 본문 응답 반환
+        return new BoardResponse(
+                board.getBoardTitle(),
+                board.getBoardContents()
+        );
     }
 }
