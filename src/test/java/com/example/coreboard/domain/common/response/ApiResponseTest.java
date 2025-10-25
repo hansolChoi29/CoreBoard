12860c06dffdd23a825faa555584dff08f17c752
hansolChoi29 <ggksthf29@gmail.com>
2025-10-25 22:20:26 +0900

    test: ApiResponse 커버리지 재측정 및 100% 확인 (코드 변경 없음)

--- src/test/java/com/example/coreboard/domain/common/response/ApiResponseTest.java
+++ src/test/java/com/example/coreboard/domain/common/response/ApiResponseTest.java
@@ -9,21 +9,14 @@ class ApiResponseTest {
     @Test
     void getMessage() {
         ApiResponse<String> response = ApiResponse.ok("dage", "성공 메시지");
-
         String msg = response.getMessage();
-        System.out.println(">>> getMessage() 결과 = " + msg);
-
         assertEquals("성공 메시지", msg);
     }
 
     @Test
     void getData() {
         ApiResponse<String> response = ApiResponse.ok("dage", "성공 메시지");
-
         String data = response.getData();
-        System.out.println(">>> getData() 결과 = " + data);
-
         assertEquals("dage", data);
     }
-    // 왜 안돼?..
 }
