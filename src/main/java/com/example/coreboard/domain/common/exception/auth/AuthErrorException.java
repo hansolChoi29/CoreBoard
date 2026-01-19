75ad0dbbf60122b14a87a6165bfc0c293dac6fc3
hansolChoi29 <ggksthf29@gmail.com>
2026-01-19 13:51:17 +0900

    feat: globalExceptionHandler field add

--- src/main/java/com/example/coreboard/domain/common/exception/auth/AuthErrorException.java
+++ src/main/java/com/example/coreboard/domain/common/exception/auth/AuthErrorException.java
@@ -5,6 +5,11 @@ import com.example.coreboard.domain.common.exception.ErrorException;
 public class AuthErrorException extends ErrorException {
 
     public AuthErrorException(AuthErrorCode authErrorCode) {
-        super(authErrorCode.getStatus(), authErrorCode.getMessage());
+        super(
+                authErrorCode.getStatus(),
+                authErrorCode.getCode(),
+                authErrorCode.getErrors(),
+                authErrorCode.getMessage()
+        );
     }
 }
