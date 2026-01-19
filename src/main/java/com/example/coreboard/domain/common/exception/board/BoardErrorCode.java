7946b67b266c7a55c88748484b9c797ca77bc93f
hansolChoi29 <ggksthf29@gmail.com>
2026-01-19 14:47:05 +0900

    feat: 예외공í통 포맷에 맞춰서 구현

--- src/main/java/com/example/coreboard/domain/common/exception/board/BoardErrorCode.java
+++ src/main/java/com/example/coreboard/domain/common/exception/board/BoardErrorCode.java
@@ -9,68 +9,101 @@ public enum BoardErrorCode {
     TITLE_AND_CONTENTS_BLANK(
             HttpStatus.BAD_REQUEST,
             400,
-            "제목과 내용은 필수입니다."
-            , List.of()
+            "제목과 내용은 필수입니다.",
+            List.of(new FieldError(
+                    "title and content",
+                    "제목과 본문은 필수입니다."
+            ))
     ),
     NOT_TITLE(
             HttpStatus.BAD_REQUEST,
             400,
             "제목은 필수입니다.",
-            List.of(new FieldError("title", "제목은 필수입니다."))
+            List.of(new FieldError(
+                    "title",
+                    "제목은 필수입니다."
+            ))
     ),
     NOT_CONTENT(
             HttpStatus.BAD_REQUEST,
             400,
             "내용은 필수입니다.",
-            List.of()
+            List.of(new FieldError(
+                    "content",
+                    "내용은 필수입니다."
+            ))
     ),
     TITLE_TOO_LONG(
             HttpStatus.BAD_REQUEST,
             400,
             "제목은 255자 이하여야 합니다.",
-            List.of()
+            List.of(new FieldError(
+                    "content",
+                    "제목은 255자 이하여야 합니다."
+            ))
     ),
     CONTENT_TOO_LONG(
             HttpStatus.BAD_REQUEST,
             400,
             "내용은 1000자 이하여야 합니다.",
-            List.of()
+            List.of(new FieldError(
+                    "content",
+                    "내용은 1000자 이하여야 합니다."
+            ))
     ),
     POST_NOT_FOUND(
             HttpStatus.NOT_FOUND,
             404,
             "존재하지 않는 게시글입니다.",
-            List.of()
+            List.of(new FieldError(
+                    "not found board",
+                    "존재하지 않는 게시글입니다."
+            ))
     ),
     TITLE_DUPLICATED(
             HttpStatus.CONFLICT,
             409,
             "이미 사용 중인 제목입니다.",
-            List.of()
+            List.of(new FieldError(
+                    "title",
+                    "이미 사용 중인 제목입니다."
+            ))
     ),
     PAGE_NOT_INTEGER(
             HttpStatus.BAD_REQUEST,
             400,
             "page는 0이상이어야 합니다.",
-            List.of()
+            List.of(new FieldError(
+                    "pageable",
+                    "page는 0이상이어야 합니다"
+            ))
     ),
     SIZE_TOO_LARGE(
             HttpStatus.BAD_REQUEST,
             400,
-            "zise는 최대 10이하이어야 합니다",
-            List.of()
+            "size는 최대 10이하이어야 합니다.",
+            List.of(new FieldError(
+                    "pageable",
+                    "size는 최대 10이하이어야 합니다."
+            ))
     ),
     SORT_DIRECTION_INVALID(
             HttpStatus.BAD_REQUEST,
             400,
             "정렬 방향은 asc 또는 desc만 허용됩니다.",
-            List.of()
+            List.of(new FieldError(
+                    "pagealbe",
+                    "정렬방향 asc/desc만 허용합니다."
+            ))
     ),
     POST_ISDELETE(
             HttpStatus.NOT_FOUND,
             404,
             "삭제된 게시글입니다.",
-            List.of()
+            List.of(new FieldError(
+                    "board",
+                    "삭제된 게시글입니다."
+            ))
     );
 
     private final HttpStatus status;
