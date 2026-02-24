7cfb4f7caa175e2bae5a10f7b39072aac76299ff
hansolChoi29 <ggksthf29@gmail.com>
2026-02-24 16:38:12 +0900

    chore: delete unused import

--- src/main/java/com/example/coreboard/domain/board/repository/BoardRepository.java
+++ src/main/java/com/example/coreboard/domain/board/repository/BoardRepository.java
@@ -1,8 +1,6 @@
 package com.example.coreboard.domain.board.repository;
 
 import com.example.coreboard.domain.board.entity.Board;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
